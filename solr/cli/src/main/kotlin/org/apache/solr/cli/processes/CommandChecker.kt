e03772916e14fa90cf3d2bbeadb96974de6d9b2c
Christos Malliaridis <c.malliaridis@gmail.com>
2024-11-15 02:23:53 +0100

    Extend and test CommandChecker

--- solr/cli/src/main/kotlin/org/apache/solr/cli/processes/CommandChecker.kt
+++ solr/cli/src/main/kotlin/org/apache/solr/cli/processes/CommandChecker.kt
@@ -22,6 +22,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.apache.solr.cli.data.OperatingSystem
 
 /**
  * Checker that holds functions for checking The existence of commands in various ways.
@@ -36,12 +37,13 @@ object CommandChecker {
      * with an error.
      */
     suspend fun commandExists(command: String): Result<Boolean> = withContext(Dispatchers.IO) {
+        if (command.isBlank()) return@withContext Result.failure(
+            IllegalArgumentException("Command $command cannot be empty."),
+        )
+
         try {
             // Run 'command -v' or 'which' with the command you want to check
-            val processBuilder = ProcessBuilder(
-                "bash", "-c",
-                "command -v $command"
-            )
+            val processBuilder = ProcessBuilder(*getCommandCheckArgs(command))
             val process = processBuilder.start()
 
             BufferedReader(InputStreamReader(process.inputStream)).use {
@@ -51,6 +53,43 @@ object CommandChecker {
             }
         } catch (exception: Exception) {
             return@withContext Result.failure(exception)
+        }
+    }
+
+    /**
+     * Fetches a [command]'s full path.
+     *
+     * @param command The command to retrieve the path.
+     * @return The full path of the command or null if the command was not found. Result is failure
+     * if execution fails to lookup the command path.
+     */
+    suspend fun getCommandPath(command: String): Result<String?> = withContext(Dispatchers.IO) {
+        if (command.isBlank()) return@withContext Result.failure(
+            IllegalArgumentException("Command $command cannot be empty."),
+        )
+
+        try {
+            // Run 'command -v' or 'which' with the command you want to check
+            val processBuilder = ProcessBuilder(*getCommandCheckArgs(command))
+            val process = processBuilder.start()
+
+            BufferedReader(InputStreamReader(process.inputStream)).use {
+                val line = it.readLine()
+                // If there’s output, the command exists; otherwise, it doesn’t
+                return@withContext Result.success(
+                    if (line != null && line.isNotBlank()) line
+                    else null
+                )
+            }
+        } catch (exception: Exception) {
+            return@withContext Result.failure(exception)
+        }
+    }
+
+    private fun getCommandCheckArgs(command: String): Array<String> {
+        return when (OperatingSystem.current) {
+            OperatingSystem.Windows -> arrayOf("where", command)
+            else -> arrayOf("which", command)
         }
     }
 }
---
+++ solr/cli/src/test/kotlin/org/apache/solr/cli/processes/CommandCheckerTest.kt
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.solr.cli.processes
+
+import java.io.File
+import java.nio.file.FileSystems
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertIs
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+import kotlinx.coroutines.test.runTest
+import kotlinx.io.files.FileSystem
+import kotlinx.io.files.Path
+import org.junit.jupiter.api.assertThrows
+
+class CommandCheckerTest {
+
+    @Test
+    fun invalidCommandNotExists() = runTest {
+        val result = CommandChecker.commandExists("somerandomcommandthatdoesnotexist")
+
+        assertTrue(result.isSuccess)
+        assertFalse(result.getOrThrow())
+    }
+
+    @Test
+    fun checkingEmptyCommandFails() = runTest {
+        val result = CommandChecker.commandExists("")
+
+        assertTrue(result.isFailure)
+        assertIs<IllegalArgumentException>(result.exceptionOrNull())
+    }
+
+    @Test
+    fun validCommandNotExists() = runTest {
+        val result = CommandChecker.commandExists("java")
+
+        assertTrue(result.isSuccess)
+        assertTrue(result.getOrThrow())
+    }
+
+    @Test
+    fun validCommandReturnsPath() = runTest {
+        val result = CommandChecker.getCommandPath("java")
+
+        assertTrue(result.isSuccess)
+        val pathString = result.getOrNull()
+        assertNotNull(pathString)
+        assertTrue(pathString.isNotBlank())
+        assertTrue(File(pathString).exists())
+    }
+
+    @Test
+    fun invalidCommandReturnsNull() = runTest {
+        val result = CommandChecker.getCommandPath("somerandomcommandthatdoesnotexist")
+
+        assertTrue(result.isSuccess)
+        val pathString = result.getOrNull()
+        assertNull(pathString)
+    }
+
+    @Test
+    fun gettingPathForEmptyCommandFails() = runTest {
+        val result = CommandChecker.getCommandPath("")
+
+        assertTrue(result.isFailure)
+        assertIs<IllegalArgumentException>(result.exceptionOrNull())
+    }
+}
